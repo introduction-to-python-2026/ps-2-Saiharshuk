36df46d071f83e96f0d37d4009e753c230958493
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-10-30 14:11:47 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,11 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    if num1 >= num2  and num1>= num3:
+      return num1
+    elif num2 >= num1 and num2 >= num3:
+      return num2
+    elif num3 >= num2 and num3 >= num1:
+      return num3     
+   
 
 def find_mean(num1, num2, num3):
     numbers = (num1, num2, num3)
