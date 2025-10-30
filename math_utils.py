0cb64def6ca30eb20d61cad3926e98c3e0fd46cb
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-10-30 14:10:50 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -2,7 +2,10 @@ def find_max_number(num1, num2, num3):
     pass  # Replace 'pass' with code
 
 def find_mean(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    numbers = (num1, num2, num3)
+    avarage = sum(numbers)/len(numbers) 
+    return avarage
+
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
